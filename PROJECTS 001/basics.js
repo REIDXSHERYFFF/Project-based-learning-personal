d331dd2081eb1a3e6f0c60bdeeb3998f12522df7
reidxsheryfff <reidxsheryfff@gmail.com>
2025-10-19 18:39:31 +0500

    adsda

--- PROJECTS 001/basics.js
+++ PROJECTS 001/basics.js
@@ -1,15 +1,48 @@
-var MyName = "John Doe";
-console.log(MyName);
+var MyName = "John Doe"; // declare a string variable named MyName
+console.log(MyName); // output the value of MyName
 
-var Myage = 30;
-console.log(Myage);
+var Myage = 30; // declare a number variable named Myage
+console.log(Myage); // output the value of Myage
 
-var sum = 10 + 20;
-console.log(sum);
+var sum = 10 + 20; // add 10 and 20
+console.log(sum); // output the result of the addition
 
-var difference = 20 - 10;
-console.log(difference);
+var difference = 20 - 10; // subtract 10 from 20
+console.log(difference); // output the result of the subtraction
 
-var division = 20 / 10;
-console.log(division);
+var division = 20 / 10; // divide 20 by 10
+console.log(division); // output the quotient
 
+var multiplication = 20 * 10; // multiply 20 by 10
+console.log(multiplication); // output the product
+
+var remainder = 20 % 10; // get the remainder of 20 divided by 10
+console.log(remainder); // output the remainder
+
+var increment = 10; // start with value 10
+console.log(increment++); // output 10, then increment to 11 (post-increment)
+
+var decrement = 10; // start with value 10
+console.log(decrement--); // output 10, then decrement to 9 (post-decrement)
+
+var x = 10; // assign 10 to x
+var y = 20; // assign 20 to y
+
+console.log(x == y); // loose equality: are values equal?
+console.log(x != y); // loose inequality: are values different?
+console.log(x > y); // is x greater than y?
+console.log(x < y); // is x less than y?
+console.log(x >= y); // is x greater than or equal to y?
+console.log(x <= y); // is x less than or equal to y?
+
+/***** ESCAPE SEQUENCE  IN STRINGS
+CODE  OUTPUT  
+\'	  single quote
+\"	  double quote
+\\	  backslash
+\n	  newline
+\r	  carriage return
+\t	  tab
+\b	  backspace
+\f	  form feed
+*****/
