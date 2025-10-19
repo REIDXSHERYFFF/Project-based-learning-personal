40cc8befbb9003f7975bc5fe1bea457129386ca2
reidxsheryfff <reidxsheryfff@gmail.com>
2025-10-19 18:18:23 +0500

    javascript

---
+++ PROJECTS 001/basics.js
@@ -0,0 +1,15 @@
+var MyName = "John Doe";
+console.log(MyName);
+
+var Myage = 30;
+console.log(Myage);
+
+var sum = 10 + 20;
+console.log(sum);
+
+var difference = 20 - 10;
+console.log(difference);
+
+var division = 20 / 10;
+console.log(division);
+
